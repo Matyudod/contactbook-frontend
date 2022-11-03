efb266417576802fb4971841ebc67ab678ae8c86
Matuydod <eratordo2001@gmail.com>
2022-11-03 14:36:24 +0700

    Project is completed

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "@/views/ContactBook.vue";
+const routes = [
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/contact/create",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true,
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+];
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
+export default router;
